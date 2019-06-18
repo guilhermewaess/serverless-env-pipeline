dbd83fa990e02b6a321ec337db8fa989df8232a7
Guilherme Waess <guilherme.waess@gmail.com>
2019-06-18 10:31:47 +0200

    configuring the repo

---
+++ src/handler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export async function helloWorld(event) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+};
